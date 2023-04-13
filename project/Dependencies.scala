fbbd9cc494fca4901ab303783b9ba47f5a4eb77c
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-04-13 20:41:27 +1000

    Upgrading dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies
 {
   val ScalaVersion = "2.13.10"
   val Http4sVersion = "0.23.18"
-  val CirceVersion = "0.14.4"
+  val CirceVersion = "0.14.5"
 
   lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % Http4sVersion
 
@@ -20,11 +20,11 @@ object Dependencies
 
   lazy val circeLiteral = "io.circe" %% "circe-literal" % CirceVersion
 
-  lazy val jodaTime = "joda-time" % "joda-time" % "2.12.2"
+  lazy val jodaTime = "joda-time" % "joda-time" % "2.12.5"
 
   lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.2"
 
-  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.4.5"
+  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.4.6"
 
   lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
 
