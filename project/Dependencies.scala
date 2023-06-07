00553d15ee59b515f0816576406a58b83e5ad60b
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-06-07 18:20:22 +1000

    Upgrading dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies
 {
   val ScalaVersion = "2.13.11"
-  val Http4sVersion = "0.23.18"
+  val Http4sVersion = "0.23.19"
   val CirceVersion = "0.14.5"
 
   lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % Http4sVersion
@@ -22,7 +22,7 @@ object Dependencies
 
   lazy val jodaTime = "joda-time" % "joda-time" % "2.12.5"
 
-  lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.3"
+  lazy val pureconfig = "com.github.pureconfig" %% "pureconfig" % "0.17.4"
 
   lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.4.7"
 
@@ -32,7 +32,7 @@ object Dependencies
 
   lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % "0.3.1"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
 
   lazy val scalaMock = "org.scalamock" %% "scalamock" % "5.2.0"
 
